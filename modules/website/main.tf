9056ef894c3e70961986a67c73528c64951f069b
Tyler Cobb <tyjcobb94@gmail.com>
2020-05-31 18:27:32 -0500

    Added more http methods on distribution

--- modules/website/main.tf
+++ modules/website/main.tf
@@ -70,7 +70,7 @@ resource "aws_cloudfront_distribution" "website_cdn" {
   }
 
   default_cache_behavior {
-    allowed_methods = ["GET", "HEAD", "POST", "PUT", "PATCH", "DELETE"]
+    allowed_methods = ["HEAD", "DELETE", "POST", "GET", "OPTIONS", "PUT", "PATCH"]
     cached_methods  = ["GET", "HEAD"]
     target_origin_id = "S3-${aws_s3_bucket.site.id}"
 
