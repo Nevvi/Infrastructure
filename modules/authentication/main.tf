0251a4e95f74fb42eaa7300254eca053714fee5d
Tyler Cobb <tyjcobb94@gmail.com>
2022-01-09 14:17:47 -0500

    Remove dead code

--- modules/authentication/main.tf
+++ modules/authentication/main.tf
@@ -1,17 +1,5 @@
 // USER POOL
 
-data "aws_lambda_function" "pre_signup_trigger" {
-  function_name = var.pre_signup_function_name
-}
-
-resource "aws_lambda_permission" "invoke_pre_signup" {
-  statement_id  = "${var.user_pool_name}-invoke-pre-signup"
-  action        = "lambda:InvokeFunction"
-  function_name = var.pre_signup_function_name
-  principal     = "cognito-idp.amazonaws.com"
-  source_arn = aws_cognito_user_pool.user_pool.arn
-}
-
 resource "aws_cognito_user_pool" "user_pool" {
   name = var.user_pool_name
   username_attributes = ["email", "phone_number"]
@@ -24,10 +12,6 @@ resource "aws_cognito_user_pool" "user_pool" {
     require_symbols = true
     temporary_password_validity_days = 7
   }
-
-//  lambda_config {
-//    pre_sign_up = data.aws_lambda_function.pre_signup_trigger.arn
-//  }
 }
 
 resource "aws_cognito_user_pool_client" "authentication_app_client" {
