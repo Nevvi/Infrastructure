51949e775b2e28a3cce2ba79762208c130837907
Tyler Standal <tyler.cobb@nevvi.net>
2024-02-25 18:07:59 -0500

    Remove phone number user pool

--- development/main.tf
+++ development/main.tf
@@ -30,27 +30,6 @@ module "user_pool" {
   api_pool_name = "nevvi-development-api-users"
 }
 
-resource "aws_cognito_user_pool" "user_pool_phone" {
-  name = "nevvi-development-public-users-v2"
-  username_attributes = ["phone_number"]
-  auto_verified_attributes = ["phone_number"]
-
-  sms_configuration {
-    external_id    = "f577c505-dfa3-437a-afcc-ecf532b24f70"
-    sns_caller_arn = "arn:aws:iam::275527036335:role/service-role/nevvi-sms-role"
-    sns_region     = "us-east-1"
-  }
-
-  password_policy {
-    minimum_length = 8
-    require_lowercase = true
-    require_uppercase = true
-    require_numbers = true
-    require_symbols = true
-    temporary_password_validity_days = 7
-  }
-}
-
 module "user_images_bucket" {
   source = "../modules/bucket"
   bucket_name = "nevvi-user-images-dev"
