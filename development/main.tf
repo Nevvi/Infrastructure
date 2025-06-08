426c4bccb719be0c5c456cbfa2edd662b4f2f872
Tyler Standal <tyler.cobb@nevvi.net>
2025-06-07 20:37:34 -0500

    Remove api user pool

--- development/main.tf
+++ development/main.tf
@@ -27,7 +27,6 @@ module "user_pool" {
   source = "../modules/authentication"
   user_pool_name = "nevvi-development-public-users"
   pre_signup_function_name = "authentication-development-preSignUpTrigger"
-  api_pool_name = "nevvi-development-api-users"
 }
 
 module "user_images_bucket" {
